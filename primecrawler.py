dbff299bb8b363f8f84bb404bae197ba4bce729e
C272 <realrussianmanconfirmed@gmail.com>
2018-03-16 10:56:07 +0000

    Adding main Python dependencies, script.
    This is the main Python script for the primecrawler. It will create a new file called "primes.txt" in your current directory.

---
+++ primecrawler.py
@@ -0,0 +1,38 @@
+#Made by C272. http://c272.ga
+
+foundPrime = 0
+#Prime array.
+while True:
+    try:
+        file = open("primes.txt", "r")
+    except FileNotFoundError:
+        file = open("primes.txt", "w")
+        file.write("2"+"\n")
+        file.flush()
+        primes = ["2"]
+        break
+    else:
+        primes = file.readlines()
+        file.close()
+        break
+    
+#Starting number.
+x = 3
+while True:
+    isPrime = 1
+    for i in primes:
+        if int(i)<int(x):
+            if x%int(i)==0:
+                isPrime = 0
+    if isPrime==0:
+        arbit = "true"
+    else:
+        print(str(x)+" is prime.")
+        foundPrime=1
+        file = open("primes.txt", "a")
+        file.write(str(x)+"\n")
+        file.close()
+        primes.append(str(x))
+    #Next number.
+    x+=1
+        
